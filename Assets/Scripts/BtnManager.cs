195e100ad8479134a6a85497b3c1d0922c908031
Ambrož Perovšek <64433302+PotatoHunter12@users.noreply.github.com>
2023-05-14 01:52:25 +0200

    whole damn clustefuck

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnManager : MonoBehaviour
+{
+    public Sprite Open;
+    public Sprite Close;
+    public Button btn;
+    public GameObject go;
+    public float x;
+    public float y;
+
+    private bool isOpen;
+    private RectTransform rt;
+
+    void Start()
+    {
+        rt = go.GetComponent<RectTransform>();
+        btn.image.sprite = Open;
+        isOpen = false;
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 1100, 0);
+    }
+
+    public void OnChatButtonPress()
+    {
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, isOpen  ? 1100 : 725, 300);
+        btn.image.sprite = isOpen ? Open : Close;
+        isOpen = !isOpen;
+    }
+    public void OnEndCall()
+    {
+        Application.Quit();
+        Debug.Log("bye");
+    }
+}
